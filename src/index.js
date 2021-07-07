ef40766510930c5bcd9d6e66faf520e4c0be41a4
guilty4all <guilty4all@gmail.com>
2021-07-07 19:46:02 +0600

    add babel config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+
+
+let o = {
+    helo: "string",
+    hi: "sdfsdfsdfsdf",
+    dfd: "dfsdfsdfsdf"
+};
+
+let newO = {
+    ...o,
+    helllo: "dfdfdfdf"
+};
+
 console.log("hello there");
+console.log('weeee live reload');
+console.log(newO);
+
+
+
+console.log(o);
